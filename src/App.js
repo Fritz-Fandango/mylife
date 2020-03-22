a1809013cb8129f2a016a70e379664263e2d917d
Martín Moreno <martin.m.moreno@icloud.com>
2020-03-21 23:42:35 -0700

    Added Proptypes and uuid package. More component adds for each section on the view.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+// Import styles
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// Import MyLife components
+import MyLifeHeader from './components/mylifeheader/MyLifeHeader';
+import MyLifeBody from './components/mylifebody/MyLifeBody';
+import MyLifeFooter from './components/mylifefooter/MyLifeFooter';
+
 function App() {
   return (
     <>
+      <CssBaseline />
+      <MyLifeHeader />
+      {/* <MyLifeBody /> */}
+      <MyLifeFooter />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Import styles
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+
 import App from './App';
+import theme from './theme/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
